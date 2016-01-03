424f1811d04224495f5d1de9942b79520b5bf0ef
fupangpangpang <jason.wfu@gmail.com>
2016-01-03 12:29:11 -0500

    JSBasic-Lesson1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var name = "Wei Fu";
+var role = "Research Associate";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+/*
+Below shall be deleted later
+*/
+
+var randomstring = "audacity";
+var newrandomstring = randomstring.slice(1,2).toUpperCase()+randomstring.slice(2);
+console.log(newrandomstring);
